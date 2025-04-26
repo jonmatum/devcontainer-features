14c84abcf05db0e49c740611049d8def53b1127f
Jonatan Mata <jonmatum@gmail.com>
2025-04-26 16:12:47 -0600

    feat(shell): add new feature and tests for default and minimal scenarios
    Signed-off-by: Jonatan Mata <jonmatum@gmail.com>

---
+++ test/shell/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Import test library
+source dev-container-features-test-lib
+
+# Example: you could check that Zsh is still installed, but Oh-My-Zsh isn't if disabled
+check "zsh installed" zsh --version
+
+# If oh-my-zsh was turned off in the scenario, you could add:
+check "oh-my-zsh NOT installed" bash -c "[[ ! -d $HOME/.oh-my-zsh ]]"
+
+reportResults
